175ea7364a692dec9297b82abae858e9bb4d5416
Maurone7b5 <mauro.fratoni@outlook.com>
2024-12-06 14:02:03 +0100

    cleaner repo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
-const https = require('https')
-const fs = require('fs')
+const https = require("https")
+const fs = require("fs")
 
 const app = express()
 const port = process.env.port || 443
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './node_modules/granular-js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'granular.js',
-  },
-};
